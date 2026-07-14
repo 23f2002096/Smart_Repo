1848b3558133acb6395a8de1546a7d917999224b
Neeraj Kumar <23f2002096@ds.study.iitm.ac.in>
2026-07-15 00:18:41 +0530

    feat: initialize flask application

---
+++ src/smart_repo/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return "<h1>Welcome to Smart Repo</h1>"
+
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
